77c74684595d9b3afe1f789b70300b5c280ad891
brenxm <brymina18@yahoo.com>
2022-07-14 22:10:54 -0500

    base style and layout implemented

---
+++ module.js
@@ -0,0 +1,45 @@
+const GameModule = (function () {
+
+    let _previousPlayer = '';
+
+    const newGame = () => {
+
+    }
+
+    const playerWon = (player) => {
+        //announce who won
+    }
+
+    const backToHome = () => {
+        //equiv to restarting game
+    }
+
+    const evaluateBoard = () => {
+        //check if someone won
+
+    }
+
+})();
+
+
+const Player = (name, assignedScoreboard) => {
+
+    let score = 0;
+
+    const select = (grid) => {
+        //player selected this grid
+    }
+
+    return { name, assignedScoreboard, select };
+}
+
+
+const uiModule = (function () {
+    const touchGrid = () => {
+
+    }
+
+    const updateScore = (player) => {
+
+    }
+})();
